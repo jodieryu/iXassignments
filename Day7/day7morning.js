cd94c98199c31587a06967822aac48b5d5e00412
Jodie Ryu <jodie.ryu@gmail.com>
2016-07-05 14:11:33 +0200

    Day 7 Morning assignment

---
+++ Day7/day7morning.js
@@ -0,0 +1,18 @@
+function getFormValues() {
+	console.log(first.value, last.value);
+}
+
+function changeColor() {
+	document.getElementById("color-div").style.color = "red";
+}
+
+function toggleImage() {
+	
+}
+
+document.getElementById("name-btn").onclick = function() {
+	getFormValues();
+};
+document.getElementById("color-btn").onclick = function() {
+	changeColor();
+};
