09b7db718bb0f6a86accc6bfa42b0b9a1c88c860
Jodie Ryu <jodie.ryu@gmail.com>
2016-07-11 13:21:20 +0200

    Day 11 Morning Assignment

---
+++ Day11/petitions_example/script.js
@@ -0,0 +1,36 @@
+var app = angular.module('petitionsApp', ['ngRoute']); 
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'MainCtrl',
+		templateUrl: 'templates/home.html',
+	})
+	$routeProvider.when('/petition/:petitionId', {
+		controller: 'PetitionCtrl',
+		templateUrl: 'templates/petition.html',
+	})
+});
+
+app.controller('MainCtrl', function($scope, $http) {
+	$http({
+		url: "https://api.whitehouse.gov/v1/petitions.json",
+		method: "GET",
+		params: {
+			title: "Education"
+		}
+	}).then(function(response) {
+		// console.log(response);
+		$scope.petitionsArray = response.data.results;
+	})
+});
+
+app.controller('PetitionCtrl', function($scope, $http, $routeParams) {
+	$http({
+		url: "https://api.whitehouse.gov/v1/petitions/" + 
+			$routeParams.petitionId +".json",
+		method: "GET"
+	}).then(function(response) {
+		// console.log(response);
+		$scope.petition = response.data.results[0];
+	})
+});
